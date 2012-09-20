15cc657673c166257638eae7cc813c3dadfb3b98
Lalle <niklas.lavrell@gmail.com>
2012-09-21 00:08:47 +0200

    about activity

---
+++ src/se/egroup/alfred/AboutActivity.java
@@ -0,0 +1,20 @@
+package se.egroup.alfred;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AboutActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_about, menu);
+        return true;
+    }
+}
--- src/se/egroup/alfred/Main.java
+++ src/se/egroup/alfred/Main.java
@@ -1,7 +1,10 @@
 package se.egroup.alfred;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * 
@@ -12,42 +15,52 @@ import android.os.Bundle;
  * 
  */
 public class Main extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	// The activity is being created.
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-    
-    @Override
-    public void onStart(){
-        // The activity is about to become visible.
-    	super.onStart();
-    }
-   
-    @Override
-    public void onResume(){
-        // The activity has become visible (it is now "resumed").
-    	super.onResume();
-    }
-    
-    @Override
-    public void onPause(){
-        // Another activity is taking focus (this activity is about to be "paused").
-    	super.onPause();
-    }
-    
-    @Override
-    public void onStop(){
-        // The activity is no longer visible (it is now "stopped")
-    	super.onStop();
-    }
-    
-    @Override
-    public void onDestroy(){
-        // The activity is about to be destroyed.
-    	super.onDestroy();
-    }
-    
-}
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// The activity is being created.
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Button button = (Button)findViewById(R.id.button_about);
+		button.setOnClickListener(new View.OnClickListener(){
+
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(Main.this, AboutActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+		
+		@Override
+		public void onStart(){
+			// The activity is about to become visible.
+			super.onStart();
+		}
+
+		@Override
+		public void onResume(){
+			// The activity has become visible (it is now "resumed").
+			super.onResume();
+		}
+
+		@Override
+		public void onPause(){
+			// Another activity is taking focus (this activity is about to be "paused").
+			super.onPause();
+		}
+
+		@Override
+		public void onStop(){
+			// The activity is no longer visible (it is now "stopped")
+			super.onStop();
+		}
+
+		@Override
+		public void onDestroy(){
+			// The activity is about to be destroyed.
+			super.onDestroy();
+		}
+
+	}
