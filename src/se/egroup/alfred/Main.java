8302567214321ca0d48a4361c58db36937465e2d
Lalle <niklas.lavrell@gmail.com>
2012-09-20 22:25:25 +0200

    added som desc.

--- src/se/egroup/alfred/Main.java
+++ src/se/egroup/alfred/Main.java
@@ -15,23 +15,39 @@ public class Main extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	// The activity is being created.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
     
     @Override
-    public void onStart(){}
+    public void onStart(){
+    	super.onStart();
+        // The activity is about to become visible.
+    }
     
     @Override
-    public void onResume(){}
+    public void onResume(){
+    	super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
     
     @Override
-    public void onPause(){}
+    public void onPause(){
+    	super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
     
     @Override
-    public void onStop(){}
+    public void onStop(){
+    	super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
     
     @Override
-    public void onDestroy(){}
+    public void onDestroy(){
+    	super.onDestroy();
+        // The activity is about to be destroyed.
+    }
     
 }
