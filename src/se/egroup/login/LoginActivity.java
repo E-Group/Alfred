e55f2aa81128db3a70248dd696b31b5dccdafc9b
Lalle <niklas.lavrell@gmail.com>
2012-09-23 11:43:06 +0200

    changed the architecture of the login functionality

--- src/se/egroup/login/LoginActivity.java
+++ src/se/egroup/login/LoginActivity.java
@@ -1,5 +1,7 @@
-package se.egroup.alfred;
+package se.egroup.login;
 
+import se.egroup.alfred.Main;
+import se.egroup.alfred.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,15 +9,29 @@ import android.view.View;
 
 public class LoginActivity extends Activity {
 
+	private boolean loggedIn = false;
+	
+	private void checkLoginStatus(){
+		if(loggedIn){
+			// TODO: call for button_login instead, but with which view?
+			// TODO: save the login state with onSaveInstanceState?
+			Intent intent = new Intent(this, Main.class);
+			startActivity(intent);
+			finish();
+		}
+	}
+	
 	public void button_login(View view){
 		Intent intent = new Intent(this, Main.class);
 		startActivity(intent);
+		finish();
 	}
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        checkLoginStatus();
     }
     
 }
